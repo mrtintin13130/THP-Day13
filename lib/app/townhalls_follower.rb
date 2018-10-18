7fe8c9bf008d8a0d9c85d4bf435483f12860b78a
mik <autoclik974@gmail.com>
2018-10-18 14:10:20 +0200

    ajout class et def

--- lib/app/townhalls_follower.rb
+++ lib/app/townhalls_follower.rb
@@ -4,22 +4,31 @@ require 'dotenv'
 Dotenv.load
 
 # quelques lignes qui enregistrent les clés d'APIs
-
+class Following
+def initialize(new_json = '')
 client = Twitter::REST::Client.new do |config|
  config.consumer_key        = ENV["TWITTER_API_KEY"]
   config.consumer_secret     = ENV["TWITTER_API_SECRET"]
   config.access_token        = ENV["TWITTER_API_TOKEN"]
   config.access_token_secret = ENV["TWITTER_API_TOKEN_SECRET"]
 end
-x = 0
-mairie = []
-while mairie[x]
-  mairie = mairie[x].gsub()
-  x += 1
-end
-return mairie
+def check
+@new_json = if new_json == ''
+                   "/../.JSON"
+                 else
+                   new_json
+                 end
+@mairie = Scrapper.new.read(@new_json)
 
-  client.follow(mairie)
-	
+
+def follow
+  @mairie.each do |commune|
+  mairie = client.user_search(commune["name"])
+  begin
+  client.follow!(mairie[0])
+  commune["mairie"] = "@" + mairie[0].screen_name
+
+  client.follow(@mairie)
+  end
   
 end
