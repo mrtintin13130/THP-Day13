df77b7d787a53be409f78838bda9f6b4e2b46641
Martin <mts13@hotmail.fr>
2018-10-18 20:49:56 +0200

    modif

--- lib/app/townhalls_adder_to_db.rb
+++ lib/app/townhalls_adder_to_db.rb
@@ -4,7 +4,7 @@ require 'csv'
 Dotenv.load
 
 # quelques lignes qui enregistrent les clés d'APIs
-new_csv =
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["TWITTER_API_KEY"]
   config.consumer_secret     = ENV["TWITTER_API_SECRET"]
@@ -12,20 +12,41 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["TWITTER_API_TOKEN_SECRET"]
 end
 
-@new_csv= "../../db/townhalls.csv"
-  
-              
-@mairie = CSV.read(@new_csv) #methode de scrapping a rajouter
-
-
-
-  @mairie.each do |commune|# iteration sur le nom de chaque commune
-  mairie = client.user_search(commune[0])# recherche par noms de commune sur twitter
-
-      begin
-        client.follow!(mairie[0])
-        commune['mairie'] = "@mairie" +  mairie[0].screen_name
-      rescue StandardError
-puts "Error: can't find a twitter account for #{commune[0]}"
+x = 0
+array_allinf = CSV.read('../../db/townhalls.csv')
+array_userid = []
+while array_allinf[x]
+  @twitter = client.user_search("Mairie " + array_allinf[x][0])
+  array_userid[x] = @twitter.first
+  puts x
+  puts array_userid[x]
+  x += 1
 end
+y = 0
+
+data = CSV.read("../../db/townhalls.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+hashed_data = data.map { |d| d.to_hash }
+
+x = 0
+y = 0
+hash = []
+hashed_data.each do
+  hash[x] = hashed_data[x].merge({:handle => array_userid[y]})
+  x += 1
+  y += 1
 end
+
+class Array
+  def to_csv(csv_filename="test.csv")
+    require 'csv'
+    CSV.open(csv_filename, "wb") do |csv|
+      csv << first.keys # adds the attributes name on the first line
+      self.each do |hash|
+        csv << hash.values
+      end
+    end
+  end
+end
+
+hash.to_csv("../../db/townhalls.csv")
+
