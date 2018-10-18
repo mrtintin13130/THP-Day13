70ad144d5bd9b933846082806e5aba09b1cc04bf
nomoon456 <nomoon4567891011@gmail.com>
2018-10-18 19:15:13 +0200

    fixing followers

--- lib/app/townhalls_follower.rb
+++ lib/app/townhalls_follower.rb
@@ -1,6 +1,5 @@
 require 'twitter'
 require 'dotenv'
- # rajouter nom fichier
 require 'csv'
 Dotenv.load
 
@@ -13,9 +12,7 @@ end
 
 array_allinf = CSV.read('../../db/townhalls.csv')
 x = 0
-
 while array_allinf[x]
-    @twitter = client.user_search("Mairie " + array_allinf[x][0])
-    puts "je follow bien"
-    client.follow(@twitter.first)
+    client.follow(array_allinf[x][2])
+    x += 1
 end
